e8df04cce52c2860e00386afe0f14552591cbead
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-07 15:11:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-add_element_to_start_of_array << "Chandler"
-end
+array 
 
 def remove_element_from_end_of_array(array)
   
