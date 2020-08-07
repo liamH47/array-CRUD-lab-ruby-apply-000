fd104e4c0cf6e934ea7a943b8205c449a3e3e2b5
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-07 15:17:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,8 +15,7 @@ array .unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  .pop(array)
-end
+  remove_element_from_end_of_array.pop
 
 def remove_element_from_start_of_array(array)
   
